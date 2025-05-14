ef322d7aa38462968eda0e5ea863ca32dd9ea185
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-14 11:41:01 +0200

    solve

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -7,11 +7,15 @@ use Illuminate\Http\Request;
 class bookingController extends Controller
 {
     //
-   function myBookings(Request $request) {
-    return $request->all();  
-}
+//    function myBookings(Request $request) {
+//     return "Name: " . $request->input('name');
+// }
 
-    function sayhasan() {
-          return "Hello";
+    function sayhasan($n) {
+          return response()->json(['data'=>['name'=> $n]]);
+     }
+
+     function login(){
+        return "<h1>Login Page</h1>";
      }
 }
