6c93943787378a31effac5dcc0609699664ed807
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-14 21:04:04 +0200

    Create file middleware

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -11,11 +11,50 @@ class bookingController extends Controller
 //     return "Name: " . $request->input('name');
 // }
 
+    // function sayHello($moto) {
+    //     return view('welcomePage')->with('nameMoto', $moto);
+    // }
     function sayHello($moto) {
-        return view('welcomePage')->with('nameMoto', $moto);
+
+        if ($moto == "Hasan") {
+            $isAuthenticated = true;
+        }  else {
+            $isAuthenticated = false;
+        }
+        
+        if ($isAuthenticated) {
+            return view('welcomePage')->with('nameMoto', $moto);
+        } else {
+            return redirect('/login');
+        }
+    }
+
+     function sayWelcome($moto) {
+
+        if ($moto == "Hasan") {
+            $isAuthenticated = true;
+        }  else {
+            $isAuthenticated = false;
+        }
+        
+        if ($isAuthenticated) {
+            return view('welcomePage')->with('nameMoto', $moto);
+        } else {
+            return redirect('/login');
+        }
     }
     
+    private function isLogin($moto){
 
+        $isAuthenticated = true;
+
+          if ($moto == "Hasan") {
+            $isAuthenticated = true;
+        }  else {
+            $isAuthenticated = false;
+        }
+
+    }
     
     // function sayhasan($n) {
     //       return response()->json(['data'=>['name'=> $n]]);
@@ -27,7 +66,8 @@ class bookingController extends Controller
 //      }
 
 
-//      function login(){
-//         return "<h1>Login Page</h1>";
-//      }
+     function login(){
+        return "<h1>Login Page</h1>";
+     }
+
 }
